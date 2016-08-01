ed16fd38749be86ab6a3e8858d6df50196861562
Administrator <Administrator@MSDN-SPECIAL>
2016-08-01 19:23:53 +0900

    머가 달라졌을까

--- wvsproject/src/main/java/jhj/scanner/bo/ScanningUrl.java
+++ wvsproject/src/main/java/jhj/scanner/bo/ScanningUrl.java
@@ -1,5 +1,9 @@
 package jhj.scanner.bo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import py4j.GatewayServer;
 
 public class ScanningUrl {
@@ -7,7 +11,6 @@ public class ScanningUrl {
 	private Stack stack;
 
 	public ScanningUrl() {
-		
 
 	}
 
@@ -26,12 +29,35 @@ public class ScanningUrl {
 
 		GatewayServer gatewayServer = new GatewayServer(new ScanningUrl(url));
 		gatewayServer.start();
-		// GatewayServer.turnLoggingOn();
-
 		System.out.println("Gateway Server Stated");
+
+		try {
+			System.out.println("**[java] python soure code execute!!");
+			String path = "C:\\Users\\Administrator\\git\\JHJ_Scanner\\wvsproject\\src\\main\\java\\scanner\\scanall.py";
+			Process p = Runtime.getRuntime().exec("cmd /c python " + path);
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			System.out.println("[java in========================");
+			//System.out.println(in);
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+			//in.close();
+			p.waitFor();
+			System.out.println("endddddd");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// GatewayServer.turnLoggingOn();
+		catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		// gatewayServer.shutdown();
 		/*
-		 * String path =
+		 * String path =1
 		 * "C:\\Users\\Administrator\\git\\JHJ_Scanner\\wvsproject\\src\\main\\java\\scanner\\scanall.py";
 		 * String path2 =
 		 * "C:\\Users\\Administrator\\Documents\\workspace-sts-3.7.3.RELEASE\\jhjScanner\\src\\scanner\\test.py";
